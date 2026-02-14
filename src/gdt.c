741a7594a4b0a89ac6ed247b9da76a04fd77c7a6
Teddyburgonde <bandama.teddy@gmail.com>
2026-02-14 11:40:55 +0100

    to do print_Stack

---
+++ src/gdt.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gdt.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 11:36:38 by tebandam          #+#    #+#             */
+/*   Updated: 2026/02/14 11:38:10 by tebandam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void print_stack(void)
+{
+	// 1. Lire le kernel stack(Registre ESP)
+	// 2. Afficher son contenu ( De maniere jolie) -> Je dois utiliser ft_printf
+
+	
+	
+}
